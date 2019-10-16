7c1e6f2e3001e758df763fbb60a51ebf336b85c6
totatoto <adam.bernouy@orange.fr>
2019-10-16 09:02:23 +0200

    ceci est un test

--- site/index.php
+++ site/index.php
@@ -12,7 +12,7 @@
     // On affiche les codes
     ?>
         <h1>Voici les codes d'accès :</h1>
-        <p><strong>CRD5-GTFT-CK65-JOPM-V29N-24G1-HH28-LLFV</strong></p>   
+        <p><strong>CRD5-GTFT-CK65-JOPM-V29N-24G1-HH28-LLF</strong></p>   
         
         <p>
         Cette page est réservée au personnel de la NASA. N'oubliez pas de la visiter régulièrement car les codes d'accès sont changés toutes les semaines.<br />
