e38f8b55378502900460ca7919d73f57fae5469c
WinnchesterHD <47182451+WinnchesterHD@users.noreply.github.com>
2019-10-22 17:32:31 +0200

    web page title change

--- site/index.php
+++ site/index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <title>Codes d'accès au serveur central de la NASA</title>
+        <title>Notre site web de fou</title>
     </head>
     <body>
     
