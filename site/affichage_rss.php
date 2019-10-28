8e6bc228a1152481698916b5bc2b5b7d499d78e0
totatoto <adam.bernouy@orange.fr>
2019-10-28 18:42:33 +0100

    test pdo

--- site/affichage_rss.php
+++ site/affichage_rss.php
@@ -8,9 +8,9 @@
         <div>
 			
 			<?php 
-			require 'DB.inc.php';
+			require ("DB.inc.php");
 
-			$db = DB->getInstance();
+			$db = DB::getInstance();
 
 			echo $db->getRSSItem("https://www.ncsc.gov.uk/api/1/services/v1/all-rss-feed.xml");
 			phpinfo();
