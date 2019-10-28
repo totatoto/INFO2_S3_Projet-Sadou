cfbe5156caea35b69803d29e07a954bba490761b
totatoto <adam.bernouy@orange.fr>
2019-10-28 18:49:14 +0100

    test pdo

--- site/affichage_rss.php
+++ site/affichage_rss.php
@@ -3,5 +3,12 @@
 
 	$db = DB::getInstance();
 
-	echo $db->getRSSItem("https://www.ncsc.gov.uk/api/1/services/v1/all-rss-feed.xml");
+	foreach ($db->getRSSItem("https://www.ncsc.gov.uk/api/1/services/v1/all-rss-feed.xml") as $item)
+				{
+					echo "<tr>\n";
+					echo "<td>".$item->getId()."</td>\n";
+					echo "<td>".$item->getTitle()."</td>\n";
+					echo "<td>".$item->getLink()."</td>\n";
+					echo "</tr>\n";
+				}
 ?>
