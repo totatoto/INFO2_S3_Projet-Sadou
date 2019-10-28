90c0e422a006687ed08ccd7a25819f90852b6d78
totatoto <adam.bernouy@orange.fr>
2019-10-28 18:46:08 +0100

    test pdo

--- site/affichage_rss.php
+++ site/affichage_rss.php
@@ -1,36 +1,7 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Notre site web</title>
-    </head>
-    <body>
-        <div>
-			
-			<?php 
-			require ("DB.inc.php");
+<?php 
+	require ("DB.inc.php");
 
-			$db = DB::getInstance();
+	$db = DB::getInstance();
 
-			echo $db->getRSSItem("https://www.ncsc.gov.uk/api/1/services/v1/all-rss-feed.xml");
-			?>
-            <!-- Insert name or a logo -->
-            <h1>CYBERNEWS</h1>
-        </div>
-        <div>
-            Des informations ici
-        </div>
-        <div>
-            Des informations ici
-        </div>
-        <div>
-            Des informations ici
-        </div>
-        <div>
-            Des informations ici
-        </div>
-        <div>
-            Des informations ici
-        </div>
-    </body>
-</html>
+	echo $db->getRSSItem("https://www.ncsc.gov.uk/api/1/services/v1/all-rss-feed.xml");
+?>
