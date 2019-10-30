d5aaa24b0ddc284c28e1eba9f52644bd916f31dd
WinnchesterHD <47182451+WinnchesterHD@users.noreply.github.com>
2019-10-30 23:57:00 +0100

    simplification de l'affichage

--- site/affichage_rss.php
+++ site/affichage_rss.php
@@ -17,20 +17,16 @@
 			echo "<h1>Items RSS de la dernière semaine</h1>\n";
 			try {
 				echo "<table>\n<tr>\n";
-				echo "<th>id</th>\n";
 				echo "<th>title</th>\n";
 				echo "<th>link</th>\n";
 				echo "<th>pub_date</th>\n";
-				echo "<th>importance</th>\n";
 				echo "</tr>\n";
 				foreach ($db->getRSSItem("http://feeds.feedburner.com/phoenixjp/") as $item)
 				{
 					echo "<tr>\n";
-					echo "<td>".$item->getId()."</td>\n";
 					echo "<td>".$item->getTitle()."</td>\n";
 					echo "<td>".$item->getLink()."</td>\n";
 					echo "<td>".$item->getPubDate()."</td>\n";
-					echo "<td>".$item->getImportance()."</td>\n";
 					echo "</tr>\n";
 				}
 				echo "</table>";
