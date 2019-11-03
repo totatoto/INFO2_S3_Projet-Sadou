c2da1c113ac6f78b9a81dfabfa511d4ef49a5a95
totatoto <adam.bernouy@orange.fr>
2019-11-03 20:55:27 +0100

    integrate js

--- site/fctAux.inc.php
+++ site/fctAux.inc.php
@@ -7,9 +7,9 @@
 		echo "\t\t<title>$title</title>\n";
 		echo "\t\t<meta charset=\"UTF-8\"/>\n";
 		if (!empty($css))
-			echo "\t\t".'<link rel="stylesheet" media="all" type="text/css" href="$css" />';
+			echo "\t\t".'<link rel="stylesheet" media="all" type="text/css" href="'.$css.'" />';
 		if (!empty($js))
-			echo "\t\t".'<script type="text/javascript" src="$js"/>';
+			echo "\t\t".'<script type="text/javascript" src="'.$js.'"/>';
 		echo "\t</head>\n";
 		echo "\t<body>\n";
 	}
