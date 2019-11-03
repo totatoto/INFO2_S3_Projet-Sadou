185ac7cf26043f372874e96520b549deb12e77e0
totatoto <adam.bernouy@orange.fr>
2019-11-03 22:07:49 +0100

    testjs

--- site/getRssItems.php
+++ site/getRssItems.php
@@ -6,14 +6,6 @@
         echo "Impossible de se connecter &agrave; la base de donn&eacute;es !";
     else
     {
-        foreach ($db->getRSSItem($_GET["link"]) as $item)
-        {
-        	echo "<tr>\n";
-        	echo "<td>".$item->getTitle()."</td>\n";
-        	echo "<td>".$item->getLink()."</td>\n";
-        	echo "<td>".$item->getPubDate()."</td>\n";
-        	echo "</tr>\n";
-        }
-        //echo json_encode($db->getRSSItem($_GET["link"]));
+        echo json_encode($db->getRSSItem($_GET["link"]));
     }
 ?>
--- site/rssItem.inc.php
+++ site/rssItem.inc.php
@@ -3,19 +3,19 @@
 /*classe permettant de representer les tuples de la table RSS_ITEM */
 class RSSItem {
       /*avec PDO, il faut que les noms attributs soient les mêmes que ceux de la table*/
-      private $id;
-      private $title;
-      private $link;
-      private $pub_date;
-      private $importance;
+      public $id;
+      public $title;
+      public $link;
+      public $pub_date;
+      public $importance;
 
       /* Les méthodes qui commencent par __ sont des methodes magiques */
       /* Elles sont appelées automatiquement par php suite à certains événements. */
       /* Ici c'est l'appel à new sur la classe qui déclenche l'exécution de la méthode */
       /* des valeurs par défaut doivent être spécifiées pour les paramètres du constructeur sinon
-      	 il y aura une erreur lorsqu'il sera appelé automatiquement par PDO 
-       */    
-      
+      	 il y aura une erreur lorsqu'il sera appelé automatiquement par PDO
+       */
+
       public function __construct($i=-1,$t="",$l="",$p="",$k=0) {
             $this->id = $i;
             $this->title = $t;
@@ -29,7 +29,7 @@ class RSSItem {
       public function getLink() { return $this->link; }
       public function getPubDate() { return $this->pub_date; }
       public function getImportance() { return $this->importance; }
-      
+
 
       public function __toString() {
             $res = "id:".$this->id."\n";
