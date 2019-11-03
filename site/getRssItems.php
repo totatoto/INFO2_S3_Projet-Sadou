c6fe762db89805adf5482642d77d3e7a557ee5e5
totatoto <adam.bernouy@orange.fr>
2019-11-03 22:03:32 +0100

    testjs

--- site/getRssItems.php
+++ site/getRssItems.php
@@ -6,7 +6,14 @@
         echo "Impossible de se connecter &agrave; la base de donn&eacute;es !";
     else
     {
-        echo $db->getRSSItem($_GET["link"]);
+        foreach ($db->getRSSItem($_GET["link"]) as $item)
+        {
+        	echo "<tr>\n";
+        	echo "<td>".$item->getTitle()."</td>\n";
+        	echo "<td>".$item->getLink()."</td>\n";
+        	echo "<td>".$item->getPubDate()."</td>\n";
+        	echo "</tr>\n";
+        }
         //echo json_encode($db->getRSSItem($_GET["link"]));
     }
 ?>
