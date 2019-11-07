ee42c3f40868b60c41902aa35833b5ba61b67f39
Openko <56291571+Openko@users.noreply.github.com>
2019-11-07 14:14:26 +0100

    html/css/JS
    WS no php

---
+++ site/JS.js
@@ -0,0 +1,15 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.opacity = "0";
+      slides[i].style.transition ="opacity 3s 1s  ease-in-out"; 
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.opacity = "1";
+  setTimeout(showSlides, 12000); // Change image every 2 seconds
+} 
