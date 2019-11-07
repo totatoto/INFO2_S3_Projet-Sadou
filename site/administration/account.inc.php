8edfde25b02b3baaedbb5bb9beb43c1ed332cb88
totatoto <adam.bernouy@orange.fr>
2019-11-07 16:34:47 +0100

    pseudo en username

--- site/administration/account.inc.php
+++ site/administration/account.inc.php
@@ -3,19 +3,19 @@
 /*classe permettant de representer les user de la table user_table */
 class Account {
       public $id;
-      public $pseudo;
+      public $username;
       public $password;
       public $status;
 
-      public function __construct($id=-1, $pseudo="", $password="", $status="user") {
+      public function __construct($id=-1, $username="", $password="", $status="user") {
             $this->id = $id;
-            $this->pseudo = $pseudo;
+            $this->username = $username;
             $this->password = $password;
             $this->status = $status;
       }
 
       public function getId() { return $this->id; }
-      public function getPseudo() { return $this->pseudo; }
+      public function getPseudo() { return $this->username; }
       public function getPassword() { return $this->password; }
       public function getStatus() { return $this->status; }
 }
