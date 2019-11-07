97dab7ec167a539dc1bffbca8c374fc2b2fbcf8c
totatoto <adam.bernouy@orange.fr>
2019-11-07 15:36:34 +0100

    test style

--- site/JS.js
+++ site/JS.js
@@ -4,18 +4,22 @@ var slideIndex = 0;
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.opacity = "0";
-      slides[i].style.transition ="opacity 3s 1s  ease-in-out"; 
+  if (slides.length != 0)
+  {
+	  for (i = 0; i < slides.length; i++) {
+		slides[i].style.opacity = "0";
+		  slides[i].style.transition ="opacity 3s 1s  ease-in-out"; 
+	  }
+	  slideIndex++;
+	  if (slideIndex > slides.length) {slideIndex = 1}
+	  console.log(slideIndex);
+	  slides[slideIndex-1].style.opacity = "1";
+		setTimeout(showSlides, 12000); // Change image every 2 seconds
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  console.log(slideIndex);
-  slides[slideIndex-1].style.opacity = "1";
-  setTimeout(showSlides, 12000); // Change image every 2 seconds
-} 
+  else
+	  setTimeout(showSlides, 1000);
+}
 
 window.addEventListener("load", function() {
-	console.log(document.getElementsByClassName("mySlides"));
   showSlides();
 },false);
