d74e9f548ff66c260f1d66b109f1613859d3b1bf
totatoto <adam.bernouy@orange.fr>
2019-11-07 16:53:33 +0100

    test

--- site/administration/administration.php
+++ site/administration/administration.php
@@ -1,6 +1,6 @@
 <?php
-include("fctAux.inc.php");
 require("DB.inc.php");
+include("fctAux.inc.php");
 
 if (isPseudoOK($_POST['pseudo_user']) && isAccountOk($_POST['pseudo_user'],$_POST['password_user']))
 {
