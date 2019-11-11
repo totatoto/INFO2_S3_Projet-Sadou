eb6ee75a1f0bff33015441f15f4e6e39aebcac4f
totatoto <adam.bernouy@orange.fr>
2019-11-11 10:30:36 +0100

    test php

--- site/administration/administration.php
+++ site/administration/administration.php
@@ -9,59 +9,8 @@ if (isPseudoOK($_POST['pseudo_user']) && isAccountOk($_POST['pseudo_user'],$_POS
 	$_SESSION['pseudo_user'] = $_POST['pseudo_user'];
 	$_SESSION['password_user'] = $_POST['password_user'];
 	$_SESSION['admin'] = true;
+	echo "test";
 }
-?>
-
-	<!DOCTYPE html>
-	<html>
-		<?php include("../site_victor/head.php"); ?>
-		<body>
-			<header>
-				<div>
-					<span style="float: right; text-align: right;">
-					<input class="favorite styledwhite" type="button" value="Log Out">
-						<?php echo "&nbsp;&nbsp;" . "User :" . "&nbsp" . $_SESSION['pseudo_user'] . "&nbsp;&nbsp;"; ?>
-					</span>
-					<?php echo "&nbsp;&nbsp;" . date("d/m/Y - h:i:s"); ?>
-				</div>
-			</header>
-
-
-
-			</br></br></br>
-			<h1>&nbsp; Administration</h1>
-			</br></br></br>
-
-
-
-			<div>
-				<span style="float: right; text-align: right;">
-				<input class="favorite styledgreen" type="button" value="Modify">
-				&nbsp;&nbsp;&nbsp;
-				<input class="favorite styledred" type="button" value="Delete">
-				&nbsp;&nbsp;&nbsp;
-				</span>
-				<p>&nbsp; Ceci est un Flux RSS 1</p>
-			</div>
-			</br></br>
-			<div>
-				<span style="float: right; text-align: right;">
-				<input class="favorite styledgreen" type="button" value="Modify">
-				&nbsp;&nbsp;&nbsp;
-				<input class="favorite styledred" type="button" value="Delete">
-				&nbsp;&nbsp;&nbsp;
-				</span>
-				<p>&nbsp; Ceci est un Flux RSS 2</p>
-			</div>
-
-
-
-
-			<?php include("../site_victor/footer.php"); ?>
-		</body>
-	</html>
-
-<?php
 else
 {
 	echo "test";
