cc08b74077094efa089f9b54719992937cc5622c
totatoto <adam.bernouy@orange.fr>
2019-11-11 10:57:26 +0100

    test php

--- site/administration/administration.php
+++ site/administration/administration.php
@@ -2,7 +2,7 @@
 require("../DB.inc.php");
 require("../fctAux.inc.php");
 
-if (isPseudoOK($_POST['pseudo_user']))
+if (isPseudoOK($_POST['pseudo_user']) && isAccountOk($_POST['pseudo_user'],$_POST['password_user']))
 {
 	session_start();
 
@@ -11,12 +11,11 @@ if (isPseudoOK($_POST['pseudo_user']))
 	$_SESSION['admin'] = true;
 	echo "test";
 }
+?>
+	<h1>test</h1>
+<?php
 else
 {
 	echo "test2";
 }
-?>
-	<h1>test</h1>
-<?php
-	echo "test3";
 ?>
