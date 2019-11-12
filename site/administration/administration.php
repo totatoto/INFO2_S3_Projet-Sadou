2f04f3d1f3c9555c4758f362ec21173355d41fa8
Eldegast <victorpoitou18@gmail.com>
2019-11-12 13:50:56 +0100

    echo ' ';

--- site/administration/administration.php
+++ site/administration/administration.php
@@ -9,56 +9,56 @@ if (isPseudoOK($_POST['pseudo_user']) && isAccountOk($_POST['pseudo_user'],$_POS
 	$_SESSION['pseudo_user'] = $_POST['pseudo_user'];
 	$_SESSION['password_user'] = $_POST['password_user'];
 	$_SESSION['admin'] = true;
-}
+
 
 
 		include("../site_victor/head.php");
-		<body>
-			<header>
-				<div>
-					<span style="float: right; text-align: right;">
-					<input class="favorite styledwhite" type="button" value="Log Out">
+		echo '<body>';
+			echo '<header>';
+				echo '<div>';
+					echo '<span style="float: right; text-align: right;">';
+					echo '<input class="favorite styledwhite" type="button" value="Log Out">';
 						echo "&nbsp;&nbsp;" . "User :" . "&nbsp" . $_SESSION['pseudo_user'] . "&nbsp;&nbsp;";
-					</span>
+					echo '</span>';
 					echo "&nbsp;&nbsp;" . date("d/m/Y - h:i:s");
-				</div>
-			</header>
+				echo '</div>';
+			echo '</header>';
 
 
 
-			</br></br></br>
-			<h1>&nbsp; Administration</h1>
-			</br></br></br>
+			echo '</br></br></br>';
+			echo '<h1>&nbsp; Administration</h1>';
+			echo '</br></br></br>';
 
 
 
-			<div>
-				<span style="float: right; text-align: right;">
-				<input class="favorite styledgreen" type="button" value="Modify">
-				&nbsp;&nbsp;&nbsp;
-				<input class="favorite styledred" type="button" value="Delete">
-				&nbsp;&nbsp;&nbsp;
-				</span>
-				<p>&nbsp; Ceci est un Flux RSS 1</p>
-			</div>
-			</br></br>
-			<div>
-				<span style="float: right; text-align: right;">
-				<input class="favorite styledgreen" type="button" value="Modify">
-				&nbsp;&nbsp;&nbsp;
-				<input class="favorite styledred" type="button" value="Delete">
-				&nbsp;&nbsp;&nbsp;
-				</span>
-				<p>&nbsp; Ceci est un Flux RSS 2</p>
-			</div>
+			echo '<div>';
+				echo '<span style="float: right; text-align: right;">';
+				echo '<input class="favorite styledgreen" type="button" value="Modify">';
+				echo '&nbsp;&nbsp;&nbsp;';
+				echo '<input class="favorite styledred" type="button" value="Delete">';
+				echo '&nbsp;&nbsp;&nbsp;';
+				echo '</span>';
+				echo '<p>&nbsp; Ceci est un Flux RSS 1</p>';
+			echo '</div>';
+			echo '</br></br>';
+			echo '<div>';
+				echo '<span style="float: right; text-align: right;">';
+				echo '<input class="favorite styledgreen" type="button" value="Modify">';
+				echo '&nbsp;&nbsp;&nbsp;';
+				echo '<input class="favorite styledred" type="button" value="Delete">';
+				echo '&nbsp;&nbsp;&nbsp;';
+				echo '</span>';
+				echo '<p>&nbsp; Ceci est un Flux RSS 2</p>';
+			echo '</div>';
 
 
 
 
 			include("../site_victor/footer.php");
-		</body>
+		echo '</body>';
 
-
+}
 else
 {
 	echo "test";
