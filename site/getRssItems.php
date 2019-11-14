74b067db94f09cfb6268e53aeae05be07895d510
WinnchesterHD <47182451+WinnchesterHD@users.noreply.github.com>
2019-11-14 12:58:39 +0100

    test

--- site/getRssItems.php
+++ site/getRssItems.php
@@ -6,6 +6,6 @@
         echo "Impossible de se connecter &agrave; la base de donn&eacute;es !";
     else
     {
-        echo substr($_GET["link"],1,strlen($_GET["link"])-1);//json_encode($db->getRSSItems());
+        echo substr($_GET["link"],1,strlen($_GET["link"])-2);//json_encode($db->getRSSItems());
     }
 ?>
