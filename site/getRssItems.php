d7e6495e2ae24fcf886f8f4110b162f5c2f6fe5c
WinnchesterHD <47182451+WinnchesterHD@users.noreply.github.com>
2019-11-14 09:24:39 +0100

    Total undo pour plusieurs liens

--- site/getRssItems.php
+++ site/getRssItems.php
@@ -6,10 +6,6 @@
         echo "Impossible de se connecter &agrave; la base de donn&eacute;es !";
     else
     {
-<<<<<<< HEAD
-        echo json_encode($db->getRSSItems($_GET["link"]));
-=======
-        echo json_encode($db->getRSSItems($_GET["links"]));
->>>>>>> 7da42cf740d58bb5ead0636c8013ffb798b3af4c
+        echo json_encode($db->getRSSItem($_GET["link"]));
     }
 ?>
