7da42cf740d58bb5ead0636c8013ffb798b3af4c
WinnchesterHD <47182451+WinnchesterHD@users.noreply.github.com>
2019-11-14 09:13:50 +0100

    Modification pour intégrer plusieurs liens source dans contenu()

--- site/affichage_rss.php
+++ site/affichage_rss.php
@@ -3,18 +3,18 @@
 	include "fctAux.inc.php";
 
 	enTete("affichage flux rss","Style.css",["rss_update.js","JS.js"],"images/vignette.png");
-	contenu("https://www.ncsc.gov.uk/api/1/services/v1/all-rss-feed.xml");
+	contenu(["https://www.ncsc.gov.uk/api/1/services/v1/all-rss-feed.xml"]);
 	pied();
 
 
-	function contenu($link)
+	function contenu($links)
 	{
 		$db = DB::getInstance();
 		if ($db == null)
 			echo "Impossible de se connecter &agrave; la base de donn&eacute;es !";
 		else
 		{
-    		echo '<div class="slideshow-container" link="'.$link.'" id="conteneurItem">';
+    		echo '<div class="slideshow-container" link="'.$links.'" id="conteneurItem">';
 			//echo "<h1>Items RSS de la dernière semaine</h1>\n";
 			//try {
 			//	echo '<table link="https://www.ncsc.gov.uk/api/1/services/v1/all-rss-feed.xml" id="tableItem"></table>';
--- site/getRssItems.php
+++ site/getRssItems.php
@@ -6,6 +6,6 @@
         echo "Impossible de se connecter &agrave; la base de donn&eacute;es !";
     else
     {
-        echo json_encode($db->getRSSItem($_GET["link"]));
+        echo json_encode($db->getRSSItems($_GET["links"]));
     }
 ?>
