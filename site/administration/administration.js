8ec82d4d3a381624bef0243c10da429b1e2ee4ab
totatoto <adam.bernouy@orange.fr>
2019-11-14 21:02:48 +0100

    test php admin

--- site/administration/administration.js
+++ site/administration/administration.js
@@ -3,14 +3,14 @@ function modify(link)
 	let inputs = document.getElementById(link).getElementsByTagName("input");
 
 	let newLink = null;
-	inputs.foreach( input =>
+	for (var i = 0; i < inputs.length; i++)
+	{
+		let input = inputs[i];
+		if (input.getAttribute("type") == "text" && input.hasAttribute("value"))
 		{
-			if (input.getAttribute("type") == "text" && input.hasAttribute("value"))
-			{
-				newLink = input.getAttribute("value");
-			}
+			newLink = input.getAttribute("value");
 		}
-	)
+	};
 
 	console.log(newLink);
 }
