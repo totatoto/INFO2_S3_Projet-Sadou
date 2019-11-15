79e5ea95b5503fedb40414920719a64844054654
totatoto <adam.bernouy@orange.fr>
2019-11-15 19:47:44 +0100

    test logout

--- site/disconnect.php
+++ site/disconnect.php
@@ -1,12 +1,13 @@
 <?php
-    //remove PHPSESSID from browser
-    if ( isset( $_COOKIE[session_name()] ) )
-    setcookie( session_name(), “”, time()-3600, “/” );
+echo $_SESSION["pseudo_user"];
     //clear session from globals
     $_SESSION = array();
     session_unset();
     //clear session from disk
     session_destroy();
+    session_write_close();
+    setcookie(session_name(),'',0,'/');
+    session_regenerate_id(true);
     echo "test";
     echo $_SESSION["pseudo_user"];
     echo "fin";
