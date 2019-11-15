d3e3319b8bb44790e3fce48825873a13976844b5
totatoto <adam.bernouy@orange.fr>
2019-11-15 19:38:54 +0100

    test php

--- site/fctAux.inc.php
+++ site/fctAux.inc.php
@@ -99,8 +99,13 @@
 	   return $data;
 	}
 
+	echo "1";
+	echo $_SESSION['pseudo_user']
+	echo "2";
 	session_start();
-	echo "la";
+	echo "3";
+	echo $_SESSION['pseudo_user']
+	echo "4";
 	if (!isConnected(false))
 		if(isPseudoOK($_POST['pseudo_user']) && isAccountOk($_POST['pseudo_user'],$_POST['password_user']))
 		{
