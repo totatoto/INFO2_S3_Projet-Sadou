39f407b40d04b309ff685442ef7940f7f9d70474
totatoto <adam.bernouy@orange.fr>
2019-11-15 08:38:55 +0100

    run.sh pour rasp

--- java/run.sh
+++ java/run.sh
@@ -1,2 +1,2 @@
-cd ./bin
-java -cp postgresql-42.2.8.jar:. RSSController $1 $2
+cd /home/pi/Documents/Projet_Sadou/INFO2_S3_Projet-Sadou/java
+java -cp postgresql-42.2.8.jar:. RSSController 1 >> /home/pi/Documents/Projet_Sadou/javaUpdates.log 2> /home/pi/Documents/Projet_Sadou/javaUpdatesError.log
