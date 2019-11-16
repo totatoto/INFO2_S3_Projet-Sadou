7cb8f5a13f1828709e734bc5ed30931cd7eceac4
WinnchesterHD <47182451+WinnchesterHD@users.noreply.github.com>
2019-11-16 16:54:23 +0100

    test submit button

--- site/administration/administration_login.php
+++ site/administration/administration_login.php
@@ -20,7 +20,7 @@
                     <input type="password" name="password_user"  />
                 </p>
                 </br></br>
-                <input type="submit" value="Send" />
+                <input id="send_button" type="submit" value="Send" />
             </form>
         </div>
         <?php include("../site_victor/footer.php"); ?>
