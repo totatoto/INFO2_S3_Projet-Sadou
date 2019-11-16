1b52dbc20f74a173f8b0e246492485c67bad44e9
WinnchesterHD <47182451+WinnchesterHD@users.noreply.github.com>
2019-11-16 17:03:03 +0100

    wrapper

--- site/administration/administration_login.php
+++ site/administration/administration_login.php
@@ -5,24 +5,26 @@
     <body>
         <?php include("../site_victor/header.php"); ?>
 
-		</br></br>
-        <div class="formulaire">
+		</br>
+        </br>
             <form method="post" action="administration.php">
                 <h1>CONNEXION</h1>
                 </br>
-                <p>
-                    <span>PSEUDO</span>
-                    <input type="text" name="pseudo_user"  />
-                </p>
-                </br>
-                <p>
-                    <span>PASSWORD</span>
-                    <input type="password" name="password_user"  />
-                </p>
+                <div class="login_content">
+                    <p>
+                        <span>PSEUDO</span>
+                        <input type="text" name="pseudo_user"  />
+                    </p>
+                    </br>
+                    <p>
+                        <span>PASSWORD</span>
+                        <input type="password" name="password_user"  />
+                    </p>
+                </div>
                 </br>
                 <input class="send_button" type="submit" value="Send" />
             </form>
-        </div>
+
         <?php include("../site_victor/footer.php"); ?>
 
     </body>
