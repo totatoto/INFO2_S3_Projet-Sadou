2f4b701980ee35b1ca90ca65bd83df19f19986aa
Eldegast <victorpoitou18@gmail.com>
2019-11-17 12:15:49 +0100

    Revert "titre"
    This reverts commit 36714cc1ee70dec37845bd4686fbbd407e24be2f.

--- site/administration/administration_login.php
+++ site/administration/administration_login.php
@@ -7,7 +7,7 @@
 
 		</br>
         </br></br></br></br>
-		<ul class="titre">Page Administration</ul>
+		<p class="titre"><FONT size="8">Page Administration</FONT></p>
 			
         <form method="post" action="administration.php">
             <h1>CONNEXION</h1>
