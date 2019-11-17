148cb5138d4cd4b7183389c70186da3b580c1551
WinnchesterHD <47182451+WinnchesterHD@users.noreply.github.com>
2019-11-17 19:56:06 +0100

    bandeau 10px margin

--- site/administration/administration.php
+++ site/administration/administration.php
@@ -19,7 +19,7 @@
 
 					include("head.php");
 					echo '<header>';
-						echo '<div>';
+						echo '<div id="bandeau">';
 							echo '<span style="float: right; text-align: right;">';
 							echo '<input class="favorite styledwhite" type="button" value="Log Out" onclick="logOut()">';
 								echo "&nbsp;&nbsp;" . "User :" . "&nbsp" . $_SESSION['pseudo_user'] . "&nbsp;&nbsp;";
