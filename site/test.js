3309291b8e5fa775ffd6ac45d7fb67cf58814549
totatoto <adam.bernouy@orange.fr>
2019-11-21 19:54:53 +0100

    test

--- site/test.js
+++ site/test.js
@@ -2,7 +2,7 @@ var expanded;
 
 function showCheckboxes(link) {
 var checkboxes = document.getElementById(link);
-if (!expanded[link] || expanded[link]=false) {
+if (!expanded[link] || expanded[link]==false) {
 checkboxes.style.display = "block";
 expanded[link] = true;
 } else {
