302448c6206cc401c93a814436b301eaeec29f74
totatoto <adam.bernouy@orange.fr>
2019-11-21 18:23:54 +0100

    f

--- site/DB.inc.php
+++ site/DB.inc.php
@@ -137,7 +137,7 @@ class DB {
 			{
 				$requete .= "( B.link_flux_rss = '".$link."' AND ARRAY[";
 				
-				$value .= json_encode($categs);
+				$value .= $categs;
 				if (is_array(categs))
 				{
 					foreach ($categs as $categ)
