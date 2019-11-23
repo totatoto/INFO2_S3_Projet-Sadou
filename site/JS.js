b24002181726d67f8b3b340aad1145ef363eab4c
WinnchesterHD <47182451+WinnchesterHD@users.noreply.github.com>
2019-11-23 18:32:05 +0100

    test

--- site/JS.js
+++ site/JS.js
@@ -14,7 +14,7 @@ function showSlides() {
 	  if (slideIndex > slides.length) {slideIndex = 1}
 	  console.log(slideIndex);
 	  slides[slideIndex-1].style.opacity = "1";
-		setTimeout(showSlides, 20000); // Change image every 2 seconds
+		setTimeout(showSlides, 2000); // Change image every 2 seconds
   }
   else
 	  setTimeout(showSlides, 1000);
