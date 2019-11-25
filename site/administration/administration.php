cbfa8617ef72ab1b754fe493fe1f594fcd7b73aa
WinnchesterHD <47182451+WinnchesterHD@users.noreply.github.com>
2019-11-25 18:03:58 +0100

    réorganiqation CSS #1

--- site/administration/administration.php
+++ site/administration/administration.php
@@ -18,16 +18,16 @@
         echo '</header>';
 
         echo '<div id="admin_choice_wrapper">';
-            echo '<p id="admin_pages">';
+            echo '<p id="admin_pages" class="choix_type_admin">';
                 echo '<a title="Administration des pages" href="administration_pages.php">Administration des pages</a>';
             echo '</p>';
-            echo '<p id="admin_flux">';
+            echo '<p id="admin_flux" class="choix_type_admin">';
                 echo '<a title="Administration des flux RSS" href="administration_flux.php">Administration des flux RSS</a>';
             echo '</p>';
-            echo '<p id="admin_categories">';
+            echo '<p id="admin_categories" class="choix_type_admin">';
                 echo '<a title="Administration des catégories" href="administration_categories.php">Administration des catégories</a>';
             echo '</p>';
-            echo '<p id="admin_comptes">';
+            echo '<p id="admin_comptes" class="choix_type_admin">';
                 echo '<a title="Administration des comptes" href="administration_users.php">Administration des comptes</a>';
             echo '</p>';
         echo '</div>';
