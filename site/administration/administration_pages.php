4bb27026ded5572e8c0cc6d27c837700de0de1d2
totatoto <adam.bernouy@orange.fr>
2019-11-27 10:32:54 +0100

    t

--- site/administration/administration_pages.php
+++ site/administration/administration_pages.php
@@ -9,68 +9,108 @@
     {
         if (isConnected(false))
 		{
-        echo '<header>';
-            echo '<div>';
-                echo '<span style="float: right; text-align: right;">';
-                echo '<input class="favorite styledwhite" type="button" value="Log Out" onclick="logOut()">';
-                    echo "&nbsp;&nbsp;" . "User :" . "&nbsp" . $_SESSION['pseudo_user'] . "&nbsp;&nbsp;";
-                echo '</span>';
-                echo "&nbsp;&nbsp;" . date("d/m/Y");
-            echo '</div>';
-        echo '</header>';
+			echo '<header>';
+				echo '<div>';
+					echo '<span style="float: right; text-align: right;">';
+					echo '<input class="favorite styledwhite" type="button" value="Log Out" onclick="logOut()">';
+						echo "&nbsp;&nbsp;" . "User :" . "&nbsp" . $_SESSION['pseudo_user'] . "&nbsp;&nbsp;";
+					echo '</span>';
+					echo "&nbsp;&nbsp;" . date("d/m/Y");
+				echo '</div>';
+			echo '</header>';
 
-        echo '<button class="bouton_retour" onclick=redirect('."'".'administration.php'."'".')>';
-            echo 'Retour';
-        echo '</button>';
+			echo '<button class="bouton_retour" onclick=redirect('."'".'administration.php'."'".')>';
+				echo 'Retour';
+			echo '</button>';
 
-        echo '<div id="choix_page">';
-            echo '<h1>Sélection</h1>';
-            echo '<ul>';
-                echo '<li>';
-                    echo '<input type="radio" name="page_selector" value="page1" checked="checked"> Page 1';
-                echo '</li>';
-                echo '<li>';
-                    echo '<input type="radio" name="page_selector" value="page2"> Page 2';
-                echo '</li>';
-                echo '<li>';
-                    echo '<input type="radio" name="page_selector" value="page3"> Page 3';
-                echo '</li>';
-                echo '<li>';
-                    echo '<input type="radio" name="page_selector" value="page4"> Page 4';
-                echo '</li>';
-            echo '</ul>';
-        echo '</div>';
+			// creation de la sélection de la page
+			echo '<div id="choix_page">';
+				echo '<h1>Sélection Pages</h1>';
+				echo '<ul>';
+				
+					$pages = getPages();
+					
+					$selectedPage = -1;
+					
+					foreach($pages as $page)
+					{
+						echo '<li>';
+						echo '<input type="radio" name="page_selector" value="page'.$page.'" '.($selectedPage == -1 ? 'checked="checked"' : '').'> Page 1';
+						echo '</li>';
+						
+						if ($selectedPage == -1)
+							$selectedPage = $page;
+					}
+				
+				echo '</ul>';
+			echo '</div>';
 
-        echo '<div class="central_wrapper" id="central_pages">';
-            echo '<div id="wrapper_droit">';
-                echo '<div id="divCurrentLinks">';
-                    if (isConnected(true))
-                    {
-                        echo '<span style="float: right; text-align: right;">';
-                        echo '<input class="favorite styledgreen" type="button" value="Modify" onclick="modifyLink('."'".$link."'".')">';
-                        echo '&nbsp;&nbsp;&nbsp;';
-                        echo '<input class="favorite styledred" type="button" value="Delete" onclick="deleteLink('."'".$link."'".')">';
-                        echo '&nbsp;&nbsp;&nbsp;';
-                        echo '</span>';
-                        echo '<input type="text" class="inputRSS" value="'.$link.'"/>';
-                    }
-                    else
-                    {
-                        echo '<p>'.$link.'</p>';
-                    }
-                echo '</div>';
+			// creation de la gestion lien-categ
+			echo '<div class="central_wrapper" id="central_pages">';
+			 echo '<div id="wrapper_droit">';
+			
+			// creation de la div currentLinks
+			echo '<div id="divCurrentLinks">';
+			
+			foreach(getLinksOfPage($selectedPage) as $link)
+			{
+				// création d'une divLink
+				echo '<div id="Link:'.$link.'">';
+				// création de l'inputLink
+				if (isConnected(true))
+					echo '<input type="text" class="inputRSS" value="'.$link.'"/>';
+				else
+					echo '<p>'.$link.'</p>';
 
-                if (isConnected(true))
-                {
-                    echo '<div id="insertLink_pages">';
-                    echo '<span style="float: right; text-align: right;">';
-                    echo '<input class="favorite styledgreen" type="button" value="Insert" onclick="insertLink()">';
-                    echo '</span>';
-                    echo '<input type="text" class="inputRSS" value="" hint="insert the Link to add here"/>';
-                    echo '</div>';
-                }
-            echo '</div>';
-        echo '</div>';
+				// création du dropDown
+				echo '<div class="multiselect">';
+				  echo '<div class="selectBox" onclick='."'".'showCheckboxes("'.$link.'")'."'".'>';
+					echo '<select>';
+					  echo '<option>Select categories</option>';
+					echo '</select>';
+					echo '<div class="overSelect"></div>';
+				  echo '</div>';
+				  echo '<div class="checkboxes" id="'.$link.'">';
+				  
+					foreach(getCategsOfLinksOfPage($selectedPage,$link) as $categ)
+					{
+						echo '<label for="'.$categ.'">';
+						echo '<input type="checkbox" id="'.$categ.'" />'.$categ.'</label>';
+					}
+					
+				  echo '</div>';
+				echo '</div>';
+
+				// création du span
+				if (isConnected(true))
+				{
+					echo '<span style="float: right; text-align: right;">';
+					echo '<input class="favorite styledgreen" type="button" value="Modify" onclick="modifyLinkOfPage('."'".$link."'".')">';
+					echo '<input class="favorite styledred" type="button" value="Delete" onclick="deleteLinkOfPage('."'".$link."'".')">';
+					echo '</span>';
+				}
+
+				echo '</div>';
+			}
+
+			echo '</div>';
+
+			// creation de la div InsertLink
+			
+			if (isConnected(true))
+			{
+				echo '<div id="insertLink_pages">';
+				
+				echo '<span style="float: right; text-align: right;">';
+				echo '<input class="favorite styledgreen" type="button" value="Insert" onclick="insertLinkOfPage('."'".$selectedPage."'".')">';
+				echo '</span>';
+				echo '<input type="text" class="inputRSS" value="" hint="insert the Link to add here"/>';
+				
+				echo '</div>';
+			}
+
+			echo '</div>';
+			echo '</div>';
         }
         else
         {
