04314c203e768b91b15ec7b87d5ecab35bf0dabc
totatoto <adam.bernouy@orange.fr>
2019-11-27 10:19:59 +0100

    t

--- site/administration/administration.js
+++ site/administration/administration.js
@@ -368,3 +368,23 @@ function redirect(link)
 	if (link)
 		document.location.href = link;
 }
+
+
+
+
+
+
+
+// JS DROPDOWN
+var expanded = new Map();
+
+function showCheckboxes(link) {
+var checkboxes = document.getElementById(link);
+if (!expanded[link] || expanded[link]==false) {
+checkboxes.style.display = "block";
+expanded[link] = true;
+} else {
+checkboxes.style.display = "none";
+expanded[link] = false;
+}
+}
