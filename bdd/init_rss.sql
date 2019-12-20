25ee0aadbe32677cd3e96e569212553a72bdb395
totatoto <adam.bernouy@orange.fr>
2019-12-20 08:21:13 +0100

    update init

--- bdd/init_rss.sql
+++ bdd/init_rss.sql
@@ -4,3 +4,8 @@ INSERT INTO public.flux_rss(link) VALUES ('https://www.zdnet.fr/blogs/cybervigil
 INSERT INTO public.flux_rss(link) VALUES ('https://www.silicon.fr/feed');
 INSERT INTO public.flux_rss(link) VALUES ('https://www.datasecuritybreach.fr/feed/');
 INSERT INTO public.flux_rss(link) VALUES ('https://www.ncsc.gov.uk/api/1/services/v1/all-rss-feed.xml');
+
+INSERT INTO public.page_links_categs(numpage, link_flux_rss, name_category)	VALUES (1, 'https://www.datasecuritybreach.fr/feed/', 'Cybersécurité');
+INSERT INTO public.page_links_categs(numpage, link_flux_rss, name_category)	VALUES (1, 'https://www.silicon.fr/feed', 'Cybersécurité');
+INSERT INTO public.page_links_categs(numpage, link_flux_rss, name_category)	VALUES (1, 'https://www.zdnet.fr/blogs/cybervigilance/rss/', 'Firewall');
+INSERT INTO public.page_links_categs(numpage, link_flux_rss)	VALUES (1, 'https://www.cert.ssi.gouv.fr/feed/');
